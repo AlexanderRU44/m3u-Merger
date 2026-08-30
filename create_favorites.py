1f7378ceae1ee4e8fe7f99d39b87e2ba52bb114b
Alexander <59964601+AlexanderRU44@users.noreply.github.com>
2026-08-30 13:22:05 +0300

    update create_favorites.py

--- create_favorites.py
+++ create_favorites.py
@@ -80,6 +80,7 @@ def load_rules_from_json():
         ],
         'loganet_tv.m3u': [
             ['Развлечение', '.Investigation Discovery HD'],
+          ['Развлечение', '.Investigation Discovery'],
             ['Кино', 'Кинохит HD'],
             ['Кино', 'Киноужас HD'],
         ],
